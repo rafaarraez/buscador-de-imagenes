11bca0830c4a009d00e9c4ba2eb5f9afadf404c5
rafael <rafa.arraez.gue@gmail.com>
2021-01-04 16:06:01 -0400

    Updates

---
+++ precache-manifest.fd958ed0c04c125a532db2cc2d4420bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0209b83ebbb3e01c1c2e60a8c07b6399",
+    "url": "/buscador-de-imagenes/index.html"
+  },
+  {
+    "revision": "1d63cb8c6da38fc7c681",
+    "url": "/buscador-de-imagenes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "459e5b7357d1b0d74bc5",
+    "url": "/buscador-de-imagenes/static/js/2.8e0bc400.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/buscador-de-imagenes/static/js/2.8e0bc400.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d63cb8c6da38fc7c681",
+    "url": "/buscador-de-imagenes/static/js/main.a443c492.chunk.js"
+  },
+  {
+    "revision": "efc67d6c5143741767b9",
+    "url": "/buscador-de-imagenes/static/js/runtime-main.6396506f.js"
+  }
+]);
